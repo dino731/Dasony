d11e474fedb5e41bb5615588e565d0184eca337a
developerchoims <developerchoims@gmail.com>
2023-08-22 17:18:57 +0900

    i "파일 생성"

---
+++ dasony-react/src/App.js
@@ -0,0 +1,32 @@
+
+import './App.css';
+import Header from './Header';
+
+function App() {
+  return (
+    <div id="window">
+      <Header/>
+      <div id="middle-window">
+        <div id="main-window">
+          asdfasdsafasdfasdsad
+          asdfsdfasdfsafsadf
+          <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+          <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+          <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+          <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+          asdfasdsafasdfasdsad
+          asdfsdfasdfsafsadf
+          <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+          <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+          <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+          <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+        </div>
+        <div id="right-window">
+          asdfkjsd;lfjasldkjfdasflkj
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
