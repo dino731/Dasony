22b8ceafd993576a4d1574f8c180b5e7d8bb8329
developerchoims <developerchoims@gmail.com>
2023-08-23 14:16:09 +0900

    리액트 수정(영역 틀 완성)
    리액트 수정(영역 틀 완성)

--- dasony-react/src/App.js
+++ dasony-react/src/App.js
@@ -1,32 +1,63 @@
 
 import './App.css';
 import Header from './Header';
+import { BrowserRouter as Router, Route, Routes, Link, useLocation } from 'react-router-dom';
+import Home from './Home';
+import Footer from './Footer';
+import Chat from './Chat';
+import {useEffect, useState} from 'react';
+import Loading from './Loading';
+
+
 
 function App() {
+  
+  const [loading, setLoading] = useState(true);
+  const getPage = async()=>{
+    setLoading(false);
+  };
+  useEffect(()=>{
+    getPage();
+  }, []);
+
   return (
-    <div id="window">
-      <Header/>
-      <div id="middle-window">
-        <div id="main-window">
-          asdfasdsafasdfasdsad
-          asdfsdfasdfsafsadf
-          <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
-          <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
-          <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
-          <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
-          asdfasdsafasdfasdsad
-          asdfsdfasdfsafsadf
-          <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
-          <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
-          <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
-          <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
-        </div>
-        <div id="right-window">
-          asdfkjsd;lfjasldkjfdasflkj
+    
+    <Router>
+    {loading ? (<Loading/>) : ''}
+
+      {/*전체 창 영역 */}
+      <div id="window">
+
+        {/*헤더 영역(사이드 바) */}
+        <Header/>
+
+        {/*중간 창 영역 */}
+        <div id="middle-window">
+          <Routes>
+
+            {/* 메인페이지 부분 */}
+            <Route path="/" element={
+                                      <div className="for-main">
+                                        <div id="main-window">
+                                          <Home/>
+                                        </div>
+                                        <div id="right-window">
+                                        </div>
+                                      </div> 
+                                    }/>
+
+            {/*메인페이지 외 부분*/ }
+            <Route path="/chat" element={<div id="normal-window"><Chat/></div>}/>
+          </Routes>
+          
+          {/*푸터 영역 */}
+          <Footer/>
         </div>
       </div>
-    </div>
+    </Router>
   );
 }
+
+
 
 export default App;
